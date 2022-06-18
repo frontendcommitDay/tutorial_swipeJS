52e458cf3f203236e02f029924378f69b429b348
hoin <72450781+lucid-jin@users.noreply.github.com>
2022-06-18 10:09:31 +0900

    update vanilla css, update functions..

--- lucid/vanilla/main.js
+++ lucid/vanilla/main.js
@@ -1,21 +1,48 @@
 import './style.css'
-import Swiper, { Navigation, Pagination } from 'swiper';
+import Swiper, {Navigation, Pagination} from 'swiper';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
-
-const swiper = new Swiper('.swiper', {
-	// configure Swiper to use modules
-	modules: [Navigation],
-	loop: true,
-	navigation: {
-		nextEl: '.swiper-button-next',
-		prevEl: '.swiper-button-prev',
-	},
-	scrollbar: {
-		el: '.swiper-scrollbar',
-	},
+const init = () => {
+	return new Swiper('.swiper', {
+		// configure Swiper to use modules
+		modules: [Navigation, Pagination],
+		loop: true,
+		navigation: {                                                               
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+		pagination: {
+			el: '.swiper-pagination',
+			type: 'bullets'
+		},
+		scrollbar: {
+			el: '.swiper-scrollbar',
+			draggable: false
+		},
+		
+		// 스피드 관련된
+		speed: 300,
+		spaceBetween: 1,
+		
+		mousewheel: {
+			invert: true
+		},
+		//
+		zoom: 2,
+		effect: 'fade'
+	});
 	
-});
+}
+
+const handleSlideChange = (e) => {
+	console.log(e)
+}
+
+const swiper = init();
+
+swiper.on('slideChange', handleSlideChange)
+
+
 
